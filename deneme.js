4d75f7fac305bfa72cdeb6169998c0ff24ef3812
Barış SAVAŞ <barissavas17@gmail.com>
2021-12-10 19:45:12 +0300

    latest

--- deneme.js
+++
@@ -1,5 +0,0 @@
-let obj ='audi,bmw,mercedes,opel,honda,toyota,ford,fiat,bentley';
-const ob2 = obj.split(',')
-let indexToyota = ob2.indexOf('toyota');
-console.log(ob2.splice(0,indexToyota))
-// console.log(ob2.filter((item)=> item.startsWith('b')));
